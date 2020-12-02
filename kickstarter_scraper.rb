70555059097c41d217e95341102e8352432e9bab
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-02 22:26:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,10 @@ def create_project_hash
   
   projects = {}
   
-  kickstarter.css()
- 
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+ projects
 end
 
 create_project_hash
